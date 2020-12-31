28dd19bde87c5564b33b7b0103f5bd5518bdf56c
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-12-31 17:51:55 +1100

    initial implementation of token authentication

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 import clientFacingError from "../utils/clientFacingError";
 
 const authErrorCode = 401;
@@ -6,6 +7,8 @@ const authErrorCode = 401;
 const getAuthMiddleware = (type) => {
 	const authMiddleware = express.Router();
 	authMiddleware.all("/*", async (req, res, next) => {
+		const employeeNumber =
+			req.body.employee_number || req.body.user || req.query.user;
 		switch (type) {
 			case "operator_access_key":
 				if (
@@ -17,10 +20,23 @@ const getAuthMiddleware = (type) => {
 					status: authErrorCode,
 					fullMessage: "Incorrect operator access key",
 				});
+			case "login":
+				if (
+					(await jwt.verify(
+						req.header.authorisation,
+						process.env.JWT_SECRET,
+					)) === employeeNumber
+				) {
+					return next();
+				}
+				return clientFacingError(res, {
+					status: authErrorCode,
+					fullMessage: "Bad authentication token",
+				});
 			default:
 				console.log(`undefined authentication type '${type}' was attempted`);
 				return clientFacingError(res, {
-					status: authErrorCode,
+					status: 500,
 					fullMessage: "Undefined authentication type",
 				});
 		}
