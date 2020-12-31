54a1487f31e2d00b522fdad01503899d7fa89a24
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-12-31 12:01:38 +1100

    corrected eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
 	env: {
-		browser: true,
 		es2021: true,
+		node: true,
 	},
 	extends: ["airbnb-base"],
 	parserOptions: {
 		ecmaVersion: 12,
 		sourceType: "module",
 	},
+	rules: {},
 	overrides: {
 		quotes: ["error", "double"],
 		indent: ["error", "tab"],
