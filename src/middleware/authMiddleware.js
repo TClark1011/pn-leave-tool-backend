a8daa03413fec19ea94ef469825dceb15ab3c242
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2020-12-31 17:16:34 +1100

    added 'operator_access_key' authentication

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,27 @@
+import express from "express";
+import clientFacingError from "../utils/clientFacingError";
+
+const authErrorCode = 401;
+
+const getAuthMiddleware = (type = "login") => {
+	const authMiddleware = express.Router();
+	authMiddleware.all("/*", async (req, res, next) => {
+		switch (type) {
+			case "operator_access_key":
+				if (
+					req.headers.operator_access_key === process.env.OPERATOR_ACCESS_KEY
+				) {
+					return next();
+				}
+				return clientFacingError(res, {
+					status: authErrorCode,
+					fullMessage: "Bad API key",
+				});
+			default:
+				return null;
+		}
+	});
+	return authMiddleware;
+};
+
+export default getAuthMiddleware;
--- src/routes/Depot.router.js
+++ src/routes/Depot.router.js
@@ -1,4 +1,5 @@
 import express from "express";
+import getAuthMiddleware from "../middleware/authMiddleware";
 import DepotModel from "../models/Depot.model";
 import createDocument from "../utils/createDocument";
 import deleteDocument from "../utils/deleteDocument";
@@ -6,8 +7,11 @@ import fetchAllDocuments from "../utils/fetchAllDocuments";
 
 const depotRouter = express.Router();
 
-depotRouter.post("/", createDocument(DepotModel));
 depotRouter.get("/", fetchAllDocuments(DepotModel));
+
+depotRouter.use("/", getAuthMiddleware("operator_access_key"));
+
+depotRouter.post("/", createDocument(DepotModel));
 depotRouter.delete("/:id", deleteDocument(DepotModel));
 
 export default depotRouter;
