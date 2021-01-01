f619ef54f1373da105c7ee92eb5650031a1643f8
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-01 17:41:21 +1100

    fixed jsdoc typo

--- src/utils/clientFacingResult.js
+++ src/utils/clientFacingResult.js
@@ -3,7 +3,7 @@
  * @param  {Express.Response} [res] - The express http response object
  * @param {Object} [options] - Options regarding the response
  * @param {number} [options.status=200] - The http status code to be used in the response
- * @param {any} [result={}] - The result of the successful operation
+ * @param {any} [options.result={}] - The result of the successful operation
  * @param {string} [option.message] - A message to be sent alongside the result. If not
  * provided, the response will consist only of the result data.
  */
