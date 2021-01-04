39dc2553299575165d51cf0426c2d513ac9f9199
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-05 08:58:07 +1100

    Implemented function to generate request json responses

---
+++ src/utils/responses/requestResponse.js
@@ -0,0 +1,37 @@
+import serverResponseTones from "../../constants/serverResponseTones";
+
+/**
+ * Generate a server response message
+ *
+ * @param {string} message The client facing message
+ * @param {string} tone The tone of the message, must be either "positive" or "negative"
+ * @param {object} [options] Optional options
+ * @param {any} [options.extraData] Extra data to be included with the response
+ * @param {string} [options.redirect] A url to redirect the user to
+ * @returns {object} A response to be returned to the client.
+ */
+const getServerResponse = (
+	message,
+	tone,
+	{ extraData = null, redirect = null }
+) => {
+	if (!serverResponseTones.includes(message)) {
+		throw Error(
+			"Response tone must be one of the following values: " +
+				JSON.stringify(serverResponseTones)
+		);
+	}
+	const response = {
+		message,
+		tone,
+	};
+	if (extraData) {
+		response.extraData = extraData;
+	}
+	if (redirect) {
+		response.redirect = redirect;
+	}
+	return response;
+};
+
+export default getServerResponse;
