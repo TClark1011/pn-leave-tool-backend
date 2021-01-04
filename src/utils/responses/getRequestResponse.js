aa057dd4ca8efcfba654a6cb4a6a284b305faebe
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-05 09:32:35 +1100

    allow data only responses

--- src/utils/responses/getRequestResponse.js
+++ src/utils/responses/getRequestResponse.js
@@ -3,23 +3,25 @@ import serverResponseTones from "../../constants/serverResponseTones";
 /**
  * Generate a server response object
  *
- * @param {string} message The client facing message
  * @param {string} tone The tone of the message, must be either "positive" or "negative"
  * @param {object} [options] Optional options
+ * @param {string} [options.message] The client facing message
  * @param {any} [options.extraData] Extra data to be included with the response
  * @param {string} [options.redirect] A url to redirect the user to
  * @returns {object} A response to be returned to the client.
  */
 const getRequestResponse = (
-	message,
 	tone,
-	{ extraData = null, redirect = null }
+	{ message = null, extraData = null, redirect = null }
 ) => {
 	if (!serverResponseTones.includes(message)) {
 		throw Error(
 			"Response tone must be one of the following values: " +
 				JSON.stringify(serverResponseTones)
 		);
+	}
+	if (extraData && !message) {
+		return extraData;
 	}
 	const response = {
 		message,
