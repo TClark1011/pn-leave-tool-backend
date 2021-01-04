3fb0fa7ce796f85ae4e48409aae1aa3b076f1557
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-05 08:33:43 +1100

    edit jsdoc comments

--- src/utils/fetchAllDocuments.js
+++ src/utils/fetchAllDocuments.js
@@ -3,22 +3,24 @@ import clientFacingResult from "./clientFacingResult";
 
 /**
  * Generates a route handling function to fetch all documents from a given model
- * @param  {Mongoose.Model} [Model] - The mongoose model from which all documents will be fetched
+ *
+ * @param  {Mongoose.Model} Model - The mongoose model from which all documents will be fetched
  * @returns {Function} A function that can be used as an express route handler and will fetch all
  * documents from given Model
  */
 const fetchDocumentsFromModel = (Model) =>
 	/**
 	 * Handles a request to fetch all documents from 'Model'
-	 * @param  {Express.Request} [req] - The http request to fetch the documents
-	 * @param  {Express.Response} [res] - The express response object
+	 *
+	 * @param  {Express.Request} req - The http request to fetch the documents
+	 * @param  {Express.Response} req - The express response object
 	 */
 	async (req, res) => {
 		try {
 			const result = await Model.find();
 			clientFacingResult(res, { result });
 		} catch (err) {
-			clientFacingError(res, { attempting: "fetch documents" });
+			clientFacingError(res, { "attempting": "fetch documents" });
 		}
 	};
 
