7c45415f14394d1d2b87532235b4afc16e974751
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-08 09:16:12 +1100

    created systemParameters to store constants

---
+++ src/constants/systemParameters.js
@@ -0,0 +1,17 @@
+/**
+ * THe number of days before leave of the corresponding status should be deleted;
+ */
+export const leaveDeleteParameters = {
+	"denied": 14,
+	"allowed": 365,
+};
+
+/**
+ * The percentage of the workforce that is required for a depot to operate
+ */
+export const requiredWorkforce = 0.9;
+
+/**
+ * The minimum number of days (from the current date) that leave can start
+ */
+export const leaveStartMinOffset = 14;
