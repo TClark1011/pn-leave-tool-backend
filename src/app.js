aaaa50f2496d61697f9782ad1342a5e0d1d990bb
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-12 08:15:33 +1100

    added error handling middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import DepotRouter from "./routes/Depot.router";
 import LeaveRouter from "./routes/Leave.router";
 import UserRouter from "./routes/User.router";
 import cors from "cors";
+import getErrorResponse from "./utils/responses/getErrorResponse";
+import "express-async-errors";
 
 const app = express();
 app.use(cors());
@@ -11,6 +13,17 @@ app.use(express.json());
 app.use("/users", UserRouter);
 app.use("/leave", LeaveRouter);
 app.use("/depots", DepotRouter);
+app.all("/*", (err, req, res, next) => {
+	if (!err) {
+		next();
+		return;
+	}
+	res
+		.status(500)
+		.json(
+			getErrorResponse({ "fullMessage": "An error ocurred", "extraData": err })
+		);
+});
 
 app.all("/*", (req, res) => {
 	res.status(404).send("Bad url");
