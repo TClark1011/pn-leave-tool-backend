ab1f478027f0eb8a3d1cadb68bbc2516a3bb27dd
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-13 10:00:41 +1100

    reverted authentication change

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -8,67 +8,48 @@ const authErrorCode = 401;
 /**
  * Generate an authentication middleware route of a specified type
  *
- * @param {string[] | string} authTypes The types of authentication to be used
+ * @param {string} type The type of authentication to be tested
  * @returns {Express.Route} A middleware route that will authenticate requests
  */
-const getAuthMiddleware = (authTypes) => {
+const getAuthMiddleware = (type) => {
 	const authMiddleware = express.Router();
 	authMiddleware.all("/*", async (req, res, next) => {
-		if (typeof authType === "string") {
-			authTypes = [authTypes];
-		}
 		const employeeNumber =
 			req.body.employee_number || req.body.user || req.query.user;
-		authTypes.forEach((type, index) => {
-			switch (type) {
-				case "operator_access_key":
-					if (
-						req.headers.operator_access_key &&
-						req.headers.operator_access_key === process.env.OPERATOR_ACCESS_KEY
-					) {
-						return next();
-					}
-					if (index === authTypes.length - 1) {
-						return res.status(authErrorCode).json(
-							getErrorResponse({
-								"fullMessage": "Incorrect operator access key",
-							})
-						);
-					}
-					break;
-				case "login":
-					if (
-						req.headers.authorisation &&
-						decodeToken(req.headers.authorisation) === employeeNumber
-					) {
-						return next();
-					}
-					if (index === authTypes.length - 1) {
-						return res.status(authErrorCode).json(
-							getServerError({
-								"fullMessage": "Bad authentication token",
-							})
-						);
-					}
-					break;
-				default:
-					if (index === authTypes.length - 1) {
-						return res.status(authErrorCode).json(
-							getServerError({
-								"fullMessage": `Undefined authentication type: '${type}`,
-							})
-						);
-					}
-			}
-		});
+		switch (type) {
+			case "operator_access_key":
+				if (
+					req.headers.operator_access_key &&
+					req.headers.operator_access_key === process.env.OPERATOR_ACCESS_KEY
+				) {
+					return next();
+				}
+				return res.status(authErrorCode).json(
+					getErrorResponse({
+						"fullMessage": "Incorrect operator access key",
+					})
+				);
+			case "login":
+				if (
+					req.headers.authorisation &&
+					decodeToken(req.headers.authorisation) === employeeNumber
+				) {
+					return next();
+				}
+				return res.status(authErrorCode).json(
+					getServerError({
+						"fullMessage": "Bad authentication token",
+					})
+				);
+			default:
+				return res.status(authErrorCode).json(
+					getServerError({
+						"fullMessage": `Undefined authentication type: '${type}`,
+					})
+				);
+		}
 	});
 	return authMiddleware;
 };
-
-/**
- * @param type
- * @param authErrorCode
- */
-const checkAuth = (type, authErrorCode) => {};
 
 export default getAuthMiddleware;
