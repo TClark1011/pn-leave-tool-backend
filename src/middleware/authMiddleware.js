278a90e5c7ef2ee90936167bcc383da33cde4dd8
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-13 09:57:42 +1100

    updated auth to handle multiple authentication types

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -8,44 +8,67 @@ const authErrorCode = 401;
 /**
  * Generate an authentication middleware route of a specified type
  *
- * @param {string} type The type of authentication to use
+ * @param {string} authTypes The type of authentication to use
  * @returns {Express.Route} A middleware route that will authenticate requests
  */
-const getAuthMiddleware = (type) => {
+const getAuthMiddleware = (authTypes) => {
 	const authMiddleware = express.Router();
 	authMiddleware.all("/*", async (req, res, next) => {
+		if (typeof authType === "string") {
+			authTypes = [authTypes];
+		}
 		const employeeNumber =
 			req.body.employee_number || req.body.user || req.query.user;
-		switch (type) {
-			case "operator_access_key":
-				if (
-					req.headers.operator_access_key === process.env.OPERATOR_ACCESS_KEY
-				) {
-					return next();
-				}
-				return res.status(authErrorCode).json(
-					getErrorResponse({
-						"fullMessage": "Incorrect operator access key",
-					})
-				);
-			case "login":
-				if (decodeToken(req.headers.authorisation) === employeeNumber) {
-					return next();
-				}
-				return res.status(authErrorCode).json(
-					getServerError({
-						"fullMessage": "Bad authentication token",
-					})
-				);
-			default:
-				return res.status(authErrorCode).json(
-					getServerError({
-						"fullMessage": `Undefined authentication type: '${type}`,
-					})
-				);
-		}
+		authTypes.forEach((type, index) => {
+			switch (type) {
+				case "operator_access_key":
+					if (
+						req.headers.operator_access_key &&
+						req.headers.operator_access_key === process.env.OPERATOR_ACCESS_KEY
+					) {
+						return next();
+					}
+					if (index === authTypes.length - 1) {
+						return res.status(authErrorCode).json(
+							getErrorResponse({
+								"fullMessage": "Incorrect operator access key",
+							})
+						);
+					}
+					break;
+				case "login":
+					if (
+						req.headers.authorisation &&
+						decodeToken(req.headers.authorisation) === employeeNumber
+					) {
+						return next();
+					}
+					if (index === authTypes.length - 1) {
+						return res.status(authErrorCode).json(
+							getServerError({
+								"fullMessage": "Bad authentication token",
+							})
+						);
+					}
+					break;
+				default:
+					if (index === authTypes.length - 1) {
+						return res.status(authErrorCode).json(
+							getServerError({
+								"fullMessage": `Undefined authentication type: '${type}`,
+							})
+						);
+					}
+			}
+		});
 	});
 	return authMiddleware;
 };
+
+/**
+ * @param type
+ * @param authErrorCode
+ */
+const checkAuth = (type, authErrorCode) => {};
 
 export default getAuthMiddleware;
--- src/routes/Leave.router.js
+++ src/routes/Leave.router.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import leaveRequestHandler from "../handlers/leave_handlers/leaveRequestHandler";
 import lmsDataHandler from "../handlers/leave_handlers/lmsDataHandler";
-// import getAuthMiddleware from "../middleware/authMiddleware";
+import getAuthMiddleware from "../middleware/authMiddleware";
 
 const LeaveRouter = Router();
 
-// LeaveRouter.use("/", getAuthMiddleware("login"));
+LeaveRouter.use("/", getAuthMiddleware(["login", "operator_access_key"]));
 
 // LeaveRouter.post("/request", validationMiddleware(registerVal));
 LeaveRouter.post("/request", leaveRequestHandler);
