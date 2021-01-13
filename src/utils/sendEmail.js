90b11107ebda3ce662fc5eb49461a87b22a65a87
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-13 12:06:00 +1100

    updated email authentication to use OAuth2

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,22 +1,39 @@
 import { format } from "date-fns";
 import nodemailer from "nodemailer";
 import hbs from "nodemailer-express-handlebars";
+import { google } from "googleapis";
+
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+	process.env.EMAIL_OAUTH_ID,
+	process.env.EMAIL_OAUTH_CLIENT_SECRET,
+	"https://developers.google.com/oauthplayground"
+);
+
+oauth2Client.setCredentials({ "refresh_token": process.env.EMAIL_REFRESH_TOKEN });
+
+const accessToken = oauth2Client.getAccessToken();
 
 /**
- * @param root0
- * @param root0.employee_number
- * @param root0.email
- * @param template.subject
- * @param template
- * @param to
- * @param action
- * @param template.template
- * @param template.context
+ * Send an email
+ *
+ * @param {string} to The email address to which to send the email
+ * @param {object} options Options about the email
+ * @param {string} options.subject The subject of the email
+ * @param {string} options.template The name of the template file (without file extension) to be used to build the email body
+ * @param {object} options.context The data used to fill in the template
+ * @returns {Promise} the promise to send the email
  */
 const sendEmail = (to, { subject, template, context }) => {
 	const transporter = nodemailer.createTransport({
 		"service": "gmail",
 		"auth": {
+			"type": "OAuth2",
+			"clientId": process.env.EMAIL_OAUTH_ID,
+			"clientSecret": process.env.EMAIL_OAUTH_CLIENT_SECRET,
+			"refreshToken": process.env.EMAIL_REFRESH_TOKEN,
+			accessToken,
 			"user": process.env.EMAIL_USER,
 			"pass": process.env.EMAIL_PASS,
 		},
