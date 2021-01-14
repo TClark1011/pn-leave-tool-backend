c03edba42b098ad401b2538df6f5d57e50d4c853
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-15 10:11:35 +1100

    Deleted validation folder

--- src/validation/schemas/loginVal.js
+++
@@ -1,7 +0,0 @@
-import { object } from "yup";
-import { employeeNumberVal, passwordVal } from "../commonAuthFields";
-
-export default object({
-	"employee_number": employeeNumberVal,
-	"password": passwordVal,
-});
