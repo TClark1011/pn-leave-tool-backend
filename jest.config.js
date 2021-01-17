e668b484766225ca6724f1f78880bfc95b6fd901
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-17 11:56:46 +1100

    fixed typo in jset config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	"testEnvironment": "node",
 	"testTimeout": 10000,
-	"testPathIgnorePatterns": ["/mode_modules/", "/build/"],
+	"testPathIgnorePatterns": ["/node_modules/", "/build/"],
 };
