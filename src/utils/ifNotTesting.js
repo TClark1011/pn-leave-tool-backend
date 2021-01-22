e8fa6b678ce0a6edd12a5d2e65dbb130d5c6704d
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-22 11:17:38 +1100

    created 'isTesting' boolean

--- src/utils/ifNotTesting.js
+++ src/utils/ifNotTesting.js
@@ -1,10 +1,12 @@
+export const isTesting = process.env.NODE_ENV === "test";
+
 /**
  * Runs provided function if the NODE_ENV environment variable is not set to test
  *
  * @param {Function} action The function to be run
  */
 const ifNotTesting = (action) => {
-	if (process.env.NODE_ENV !== "test") {
+	if (!isTesting) {
 		action();
 	}
 };
