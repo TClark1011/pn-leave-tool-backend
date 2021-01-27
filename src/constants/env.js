05d72654a15b7ea50414ee27499c962eea9ef451
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-01-28 10:08:27 +1100

    .

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,6 +1,8 @@
 import toBoolean from "to-boolean";
 import { isTesting } from "../utils/ifNotTesting";
 
+export const ENV_VALIDATE_EMAIL = process.env.VALIDATE_EMAIL;
+
 export const validateEmails =
-	!process.env.VALIDATE_EMAIL ||
-	(toBoolean(process.env.VALIDATE_EMAIL) && !isTesting);
+	!ENV_VALIDATE_EMAIL ||
+	(toBoolean(process.env.ENV_VALIDATE_EMAIL) && !isTesting);
