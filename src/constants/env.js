aad55e5c4403e3af66ab52c9e318aabfba34cff3
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-02-05 12:32:22 +1100

    exported all env variables from env.js

--- src/constants/env.js
+++ src/constants/env.js
@@ -6,3 +6,19 @@ export const ENV_VALIDATE_EMAIL = process.env.VALIDATE_EMAIL;
 export const validateEmails =
 	!ENV_VALIDATE_EMAIL ||
 	(ENV_VALIDATE_EMAIL && toBoolean(ENV_VALIDATE_EMAIL) && !isTesting);
+
+export const PORT = process.env.PORT || 4004;
+
+export const EMAIL_ADDRESS = process.env.EMAIL_ADDRESS;
+export const EMAIL_USER = process.env.EMAIL_USER;
+export const EMAIL_OAUTH_ID = process.env.EMAIL_OAUTH_ID;
+export const EMAIL_OAUTH_CLIENT_SECRET = process.env.EMAIL_OAUTH_CLIENT_SECRET;
+export const EMAIL_REFRESH_TOKEN = process.env.EMAIL_REFRESH_TOKEN;
+export const EMAIL_TESTING_API_KEY = process.env.EMAIL_TESTING_API_KEY;
+//? Key for the api used to test emails
+export const SUPPORT_EMAIL = process.env.SUPPORT_EMAIL;
+
+export const OPERATOR_ACCESS_KEY = process.env.OPERATOR_ACCESS_KEY;
+
+export const BACKEND_URL = BACKEND_URL || `http://localhost:${PORT}`;
+export const FRONTEND_URL = FRONTEND_URL || "http://localhost:3000";
