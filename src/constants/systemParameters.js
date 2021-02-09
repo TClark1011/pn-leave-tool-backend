a73d5347f086fa04324e15259052a1e5776c1919
Thomas Clark <48699309+TClark1011@users.noreply.github.com>
2021-02-09 12:51:15 +1100

    removed unused constants

--- src/constants/systemParameters.js
+++ src/constants/systemParameters.js
@@ -2,16 +2,3 @@
  * The percentage of the workforce that is required for a depot to operate
  */
 export const requiredWorkforce = 0.9;
-
-/**
- * The minimum number of days (from the current date) that leave can start
- */
-export const leaveStartMinOffset = 14;
-
-/**
- * Minimum/maximum length (in days) of annual leave
- */
-export const leaveLength = {
-	"min": 1,
-	"max": 100,
-};
